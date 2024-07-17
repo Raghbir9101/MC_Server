36330f48edf7b66a3862e089b1cc654650ed6951
raghbirtestttttttt <raghbir@ceoitbox.in>
2024-07-17 13:00:12 +0000

    add

--- start.js
+++ start.js
@@ -2,17 +2,21 @@ const { exec } = require('child_process');
 
 // Function to execute a command
 function executeCommand(command, callback) {
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error executing command: ${command}`, error);
-      return;
-    }
-    if (stderr) {
-      console.warn(`Warning for command: ${command}`, stderr);
-    }
-    console.log(`Output for command: ${command}`, stdout);
-    callback();
-  });
+  try {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing command: ${command}`, error);
+        return;
+      }
+      if (stderr) {
+        console.warn(`Warning for command: ${command}`, stderr);
+      }
+      console.log(`Output for command: ${command}`, stdout);
+      callback();
+    });
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 // Function to check if there are changes to commit
